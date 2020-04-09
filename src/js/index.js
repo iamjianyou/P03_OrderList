58bff0abe3d2b59ca9c45bb3d57dbc9124d05a15
andrewjianyouday <andrewjianyouday@gmail.com>
2020-04-09 17:07:53 +0100

    add event listener for changing servings count

--- src/js/index.js
+++ src/js/index.js
@@ -126,3 +126,24 @@ const controlRecipe = async () => {
 // window.addEventListener('load', controlRecipe);
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+/** Handling recipe button clicks */
+
+// attach event listener to elements recipe
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        // decrease button is clicked
+        if (state.recipe.servings > 1){
+            state.recipe.updateServings('dec');
+
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+        
+    }else if (e.target.matches('.btn-increase, .btn-increase *')){
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+
+    console.log(state.recipe);
+});
