70b2c2d9ca1963eff721cfee547d5c3c9b352232
andrewjianyouday <andrewjianyouday@gmail.com>
2020-04-10 20:16:42 +0100

    check likes status when loading

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -42,7 +42,7 @@ const createIngredient = ingredient => `
         </div>
     </li>
 `;
-export const renderRecipe = recipe => {
+export const renderRecipe = (recipe, isliked) => {
     const markup = `
     <figure class="recipe__fig">
         <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
@@ -82,7 +82,7 @@ export const renderRecipe = recipe => {
         </div>
         <button class="recipe__love">
             <svg class="header__likes">
-                <use href="img/icons.svg#icon-heart-outlined"></use>
+                <use href="img/icons.svg#icon-heart${isliked ? '' : '-outlined'}"></use>
             </svg>
         </button>
     </div>
