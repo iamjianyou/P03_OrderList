0af56a5c5b78a9b64d03d5a528f333d5341bc1a6
andrewjianyouday <andrewjianyouday@gmail.com>
2020-04-10 11:51:55 +0100

    create shopping list view, render/delete items

---
+++ src/js/views/ListView.js
@@ -0,0 +1,29 @@
+import {elements} from './base'
+
+
+/** render shoping items */
+export const renderItem = item => {
+    `
+    <li class="shopping__item" data-itemid=${item.id}>
+        <div class="shopping__count">
+            <input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
+            <p>${item.unit}</p>
+        </div>
+        <p class="shopping__description">${item.ingredient}</p>
+        <button class="shopping__delete btn-tiny">
+            <svg>
+                <use href="img/icons.svg#icon-circle-with-cross"></use>
+            </svg>
+        </button>
+    </li>
+
+    `;
+    elements.shopping.insertAdjacentHTML('beforeend', markup);
+}
+
+/** Delete item in shopping list */
+export const deleteItem = id => {
+
+    const item = document.querySelector(`[data-item ="${id}]"`);
+    item.parentElement.removeChild(item);
+}
